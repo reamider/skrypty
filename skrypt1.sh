734d13fc1974615149ee2db04428768bec8e2cf4
reamider <sebastian.matyjas@gmail.com>
2014-01-12 04:15:06 +0100

    Update skrypt1.sh

--- skrypt1.sh
+++ skrypt1.sh
@@ -3,5 +3,5 @@ echo "Uruchomiono skrypt o nazwie $0"
 echo "Pierwszy parametr to $1"
 echo "Drugi parametr to $2"
 echo "Trzeci parametr to $3"
-echo "Liczba parametrow: $#
+echo "Liczba parametrow: $#"
 echo "Wszystkie parametry wywolania: $@" 
